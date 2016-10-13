2e9463faed8fcc26e39827696eb1cc6956522877
zainmustafa <zmdhillo@gmail.com>
2016-10-14 03:03:38 +0500

    added bubble chart to app.

--- src/client/app/admin/admin.controller.js
+++ src/client/app/admin/admin.controller.js
@@ -24,46 +24,84 @@
 
     }
     function loadCSV(){
-      var svg = d3.select("svg"),
-        margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom;
 
-      var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-        y = d3.scaleLinear().rangeRound([height, 0]);
 
-      var g = svg.append("g")
+      var margin = {top: 40, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+      var formatPercent = d3.format(".0%");
+
+      var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+      var y = d3.scale.linear()
+        .range([height, 0]);
+
+      var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+      var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(formatPercent);
+
+      var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+          return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
+        });
+
+      var svg = d3.select("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      d3.csv("app/csv/data.csv", function(error, data) {
-        if (error) throw error;
+      svg.call(tip);
+
+      d3.csv("app/csv/data.csv", type, function(error, data) {
+
+        debugger;
         x.domain(data.map(function(d) { return d.letter; }));
         y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
-        g.append("g")
-          .attr("class", "axis axis--x")
+        svg.append("g")
+          .attr("class", "x axis")
           .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x));
+          .call(xAxis);
 
-        g.append("g")
-          .attr("class", "axis axis--y")
-          .call(d3.axisLeft(y).ticks(10, "%"))
+        svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
           .append("text")
           .attr("transform", "rotate(-90)")
           .attr("y", 6)
-          .attr("dy", "0.71em")
-          .attr("text-anchor", "end")
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
           .text("Frequency");
 
-        g.selectAll(".bar")
+        svg.selectAll(".bar")
           .data(data)
           .enter().append("rect")
           .attr("class", "bar")
           .attr("x", function(d) { return x(d.letter); })
+          .attr("width", x.rangeBand())
           .attr("y", function(d) { return y(d.frequency); })
-          .attr("width", x.bandwidth())
-          .attr("height", function(d) { return height - y(d.frequency); });
+          .attr("height", function(d) { return height - y(d.frequency); })
+          .on('mouseover', tip.show)
+          .on('mouseout', tip.hide)
+
       });
+
+      function type(d) {
+        d.frequency = +d.frequency;
+        return d;
+      }
+
+
       return true;
     }
   }
