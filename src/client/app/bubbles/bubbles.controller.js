57a3e6ece4d0266442a39572f55d4b9cf1039111
zainmustafa <zmdhillo@gmail.com>
2016-10-20 23:49:07 +0500

    Transitions and Dropdowns with details for bubbles.

--- src/client/app/bubbles/bubbles.controller.js
+++ src/client/app/bubbles/bubbles.controller.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular
-    .module('app.bubbles')
+    .module('app.bubbles',[])
     .controller('BubblesController', BubblesController);
 
   BubblesController.$inject = ['$q', 'logger'];
@@ -10,6 +10,25 @@
   function BubblesController($q, logger) {
     var vm = this;
     vm.title = 'Bubbles';
+    vm.radius = "Walc";
+    vm.bubbleTitle = "guardian";
+    vm.properties = [
+      "Dalc",
+      "Walc",
+      "age",
+      "failures",
+      "studytime"
+    ];
+    vm.yAxisArray = ["age", "failures", "guardian","studytime"];
+    vm.titleDetails = {  studytime:"weekly study time",
+                        failures:"number of past class failures",
+                        guardian:"student's guardian",
+                        age:"student's age"};
+    vm.radisuDetails = {Dalc : "workday alcohol consumption 1-5",
+      Walc : "weekend alcohol consumption 1-5",
+      age:"student's age",
+      studytime:"weekly study time",
+      failures:"number of past class failures"};
 
     activate();
 
@@ -24,8 +43,9 @@
 
 
     function loadCSV(){
-
       /*Source : https://jrue.github.io/coding/2014/exercises/basicbubblepackchart/*/
+
+      d3.selectAll("svg > *").remove();
 
       var diameter = 500, //max size of the bubbles
         color    = d3.scale.category20b(); //color category
@@ -41,20 +61,23 @@
       d3.csv("app/csv/student.csv", function(error, data){
 
         //convert numerical values from strings to numbers
-        data = data.map(function(d){ d.value = +d["age"]; return d; });
+        data = data.map(function(d){ d.value = +d[vm.radius]; return d; });
 
         //bubbles needs very specific format, convert data to this.
         var nodes = bubble.nodes({children:data}).filter(function(d) { return !d.children; });
 
         //setup the chart
         var bubbles = svg.append("g")
-          .attr("transform", "translate(250,50)")
+          .attr("transform", "translate(250,0)")
           .selectAll(".bubble")
           .data(nodes)
           .enter();
 
         //create the bubbles
         bubbles.append("circle")
+          .attr("r","0")
+          .transition()
+          .delay(1000)
           .attr("r", function(d){ return d.r; })
           .attr("cx", function(d){ return d.x; })
           .attr("cy", function(d){ return d.y; })
@@ -65,7 +88,7 @@
           .attr("x", function(d){ return d.x; })
           .attr("y", function(d){ return d.y + 5; })
           .attr("text-anchor", "middle")
-          .text(function(d){ return d["age"]; })
+          .text(function(d){ return d[vm.bubbleTitle].substring(0, d.r / 3); })
           .style({
             "fill":"white",
             "font-family":"Helvetica Neue, Helvetica, Arial, san-serif",
@@ -73,5 +96,8 @@
           });
       })
     }
+    vm.reDraw = function () {
+      loadCSV();
+    };
   }
 })();
